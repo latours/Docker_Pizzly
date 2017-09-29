26c01cef9dfb62c6e547c9e05053c2776ac132fc
Sara Latour <latours@uoguelph.ca>
2017-09-29 15:21:53 -0400

    Update batch_pizzly.sh

--- batch_pizzly.sh
+++ batch_pizzly.sh
@@ -3,7 +3,6 @@
 ###CONTACT: saralatour@outlook.com
 ###UPDATED:21/09/17
 
-
 #Customization:
 
 #Path to Pizzly 
@@ -23,6 +22,7 @@ GTF=/home/saral/Documents/Fusioncallers/reference_genome/Homo_sapiens.GRCh38.90.
 cat << "EOF"
 :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 EOF
+
 #Maintainer:
 echo -e "\nPizzly Fusion Caller for RNA-seq Read Pairs:"
 echo -e "\nMAINTAINER:\nSara Latour"
@@ -33,6 +33,7 @@ echo -e "\nDATE(dd_mm_yyyy):\n$now\n"
 cat << "EOF"
 :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 EOF
+
 #CMD Line Arguments:
 inputdir=$1
 outputdir=$2
@@ -67,6 +68,7 @@ mkdir -p $outputdir/Fastas
 mkdir -p $outputdir/Json
 mkdir -p $outputdir/Pizzly_Run_Information
 mkdir -p $outputdir/FusionCaller_Output_"$now"
+
 #Create File Lists - Note to self: Change it to find later.
 cd $inputdir
 find *."fastq.gz" > $outputdir/filenames.txt 
@@ -84,13 +86,10 @@ echo -e "Completed Index Step!\n"
 #RUN Kallisto Quant & Pizzly
 cat filenames.txt | while read line; do sed 's/R[1,2]_001.fastq.gz//g' ; done > $outputdir/kallisto_list.txt
 sort -u $outputdir/kallisto_list.txt > $outputdir/kallisto_unique_list.txt
-
 echo -e "Running Pizzly Fusion Caller on the following RNA-seq Pairs IDs...\n"
 cat $outputdir/kallisto_unique_list.txt
 echo -e "\nStarting Kallisto and Pizzly runs for files from $inputdir directory now...\n"
-
 cat $outputdir/kallisto_unique_list.txt | while read line; do echo "$line"; $kallistopath/kallisto quant -i $outputdir/index.idx --fusion -o $outputdir/"$line"pizzly_out $inputdir/"$line"R1_001.fastq.gz $inputdir/"$line"R2_001.fastq.gz ; $pizzlypath/build/pizzly -k 31 --gtf $GTF --cache index.cache.txt --align-score 2 --insert-size 400 --fasta $cdna --output "$line"pizzly_out $outputdir/"$line"pizzly_out/fusion.txt ; $pizzlypath/scripts/flatten_json.py "$line"pizzly_out.json > "$line""$now".txt;tr ' ' '\t'<"$line""$now".txt > "$line""$now"_final.txt ; awk '{print $1,$2,$3,$4,$5,$6}' "$line""$now"_final.txt > "$line""$now"_no_filter.txt ; awk '{if ($5&&$6 != 0 ) print $1,$2,$3,$4,$5,$6;}' "$line""$now"_final.txt > "$line""$now"_sc_pc_filter.txt ; rm "$line""$now".txt; done
-
 cat << "EOF"
 :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 EOF
